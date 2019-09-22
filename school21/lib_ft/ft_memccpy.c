c1f5c914a5359a48c5e7590fae80d6ca3922de12
danillenname <55623401+danillenname@users.noreply.github.com>
2019-09-22 11:20:34 +0300

    true var

--- school21/lib_ft/ft_memccpy.c
+++ school21/lib_ft/ft_memccpy.c
@@ -1,18 +1,21 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 void *ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	char *ptr;
 	const char *ptr2;
+	int i;
 
+	i = 1;
 	ptr = (char *)dst;
 	ptr2 = (char *)src;
 	while (n-- > 0)
 	{
-		if (*(ptr2) == c)
-			return (0);
 		*(ptr++) = *(ptr2++);
+		i++;
+		if (*(ptr2) == c)
+			return ((void *)(dst + i));
 	}
-	return (dst);
+	return (NULL);
 }
